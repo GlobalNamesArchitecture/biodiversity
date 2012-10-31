035e036b137a8741a3ed526d03ee987348791bbc
Dmitry Mozzherin <dmozzherin@gmail.com>
2012-10-31 14:54:11 -0400

    capitalization fix

--- lib/biodiversity/parser.rb
+++ lib/biodiversity/parser.rb
@@ -66,6 +66,36 @@ end
 class ScientificNameParser
   VERSION = open(File.join(File.dirname(__FILE__), '..', '..', 'VERSION')).readline.strip
   
+  def self.fix_case(name_string)
+    name_ary = name_string.split(/\s+/)
+    words_num = name_ary.size
+    res = nil
+    if words_num == 1
+      res = name_ary[0].gsub(/[\(\)\{\}]/, '')
+      if res.size > 1
+        res = UnicodeUtils.upcase(res[0]) + UnicodeUtils.downcase(res[1..-1])
+      else
+        res = nil
+      end
+    else
+      if name_ary[0].size > 1
+        word1 = UnicodeUtils.upcase(name_ary[0][0]) + UnicodeUtils.downcase(name_ary[0][1..-1])
+      else
+        word1 = name_ary[0]
+      end
+      if name_ary[1].match(/^\(/)
+        word2 = name_ary[1].gsub(/\)$/, '') + ")"
+        word2 = word2[0] + UnicodeUtils.upcase(word2[1]) + UnicodeUtils.downcase(word2[2..-1])
+      else
+        word2 = UnicodeUtils.downcase(name_ary[1])
+      end
+      res = word1 + " " + word2 + " " + name_ary[2..-1].map { |w| UnicodeUtils.downcase(w) }.join(" ")
+      res.strip!
+    end
+    res
+  end
+
+  
   def initialize
     @verbatim = ''
     @clean = ScientificNameCleanParser.new
