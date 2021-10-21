aecf54ea0505234314447815bd0bf7b8908eef6e
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-10-21 04:01:11 -0500

    update gnparser to v1.4.2

--- lib/biodiversity/version.rb
+++ lib/biodiversity/version.rb
@@ -2,8 +2,8 @@
 
 # Biodiversity module provides a namespace for scientific name parser.
 module Biodiversity
-  VERSION = '5.3.3'
-  GNPARSER_VERSION = 'GNparser 1.3.3'
+  VERSION = '5.3.4'
+  GNPARSER_VERSION = 'GNparser 1.4.2'
 
   def self.version
     VERSION
